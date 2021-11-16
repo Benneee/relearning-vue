a955d997a950248f9d02d44c9674cb33065026b2
Benedict <benedictiknkeonye@gmail.com>
2021-11-16 15:46:01 +0100

    Feat: working with data and methods

---
+++ basics-app/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: 'Finish the course',
+      vueLink: 'https://vuejs.org',
+    };
+  },
+
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue';
+      } else {
+        return 'Finish this course';
+      }
+    },
+  },
+});
+
+// Since we only want to control this section of the application
+app.mount('#user-goal');
