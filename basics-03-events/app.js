dc562f03671df81202436f6ba7040f8cda919ccc
Benedict <benedictiknkeonye@gmail.com>
2021-11-16 21:28:19 +0100

    Feat: cleaner way of handling events

--- basics-03-events/app.js
+++ basics-03-events/app.js
@@ -4,6 +4,19 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+
+  methods: {
+    increaseCount() {
+      this.counter++;
+    },
+
+    decreaseCount() {
+      //  I don't want negative numbers
+      if (this.counter >= 1) {
+        this.counter--;
+      }
+    },
+  },
 });
 
 app.mount('#events');
