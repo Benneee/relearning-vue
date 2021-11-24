30548b7c1aa7b88aeb16a9e3b698d61eb67c4c23
Benedict <benedictiknkeonye@gmail.com>
2021-11-24 01:03:18 +0100

    Feat: removing list items

--- lists-cond-01/app.js
+++ lists-cond-01/app.js
@@ -11,6 +11,9 @@ const app = Vue.createApp({
       this.goals.push(this.enteredGoal);
       this.enteredGoal = '';
     },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
   },
 });
 
