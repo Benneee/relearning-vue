e11c41d501c34aab85a532452627389047a6c83f
Benedict <benedictiknkeonye@gmail.com>
2021-11-24 11:13:25 +0100

    Feat: special attack functionality

--- monster-slayer/app.js
+++ monster-slayer/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
 
@@ -20,10 +21,15 @@ const app = Vue.createApp({
     playerHealthBarStyles() {
       return { width: this.playerHealth + '%' };
     },
+
+    canUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
 
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -32,6 +38,14 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+
+    // The special attack should only be available every 3 rounds
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
