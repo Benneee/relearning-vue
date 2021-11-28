6c69219bc76fd4438ac0c138633b3cc5029e97d4
Benedict <benedictiknkeonye@gmail.com>
2021-11-28 04:27:03 +0100

    Feat: communication between components - props

---
+++ new-vue-project/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'FriendContact',
+  props: {
+    friend: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style></style>
