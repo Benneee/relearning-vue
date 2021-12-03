cd3e060d34d613441624de124a948c28004d706e
Benedict <benedictiknkeonye@gmail.com>
2021-12-03 05:19:48 +0100

    Feat: provide and inject

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <KnowledgeGrid @select-topic="$emit('select-topic', $event)" />
+  </section>
+</template>
+
+<script>
+import KnowledgeGrid from './KnowledgeGrid.vue';
+
+export default {
+  name: 'KnowledgeBase',
+  components: {
+    KnowledgeGrid,
+  },
+  emits: ['select-topic'],
+};
+</script>
---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <KnowledgeElement
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    />
+  </ul>
+</template>
+
+<script>
+import KnowledgeElement from './KnowledgeElement.vue';
+export default {
+  name: 'KnowledgeGrid',
+
+  components: {
+    KnowledgeElement,
+  },
+
+  inject: ['topics'],
+  emits: ['select-topic'],
+};
+</script>
