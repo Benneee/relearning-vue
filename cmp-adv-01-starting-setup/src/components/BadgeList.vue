dd4066fe1258dac8e80385eee41f17c5eb8e32cc
Benedict <benedictiknkeonye@gmail.com>
2021-12-08 22:23:08 +0100

    Feat: slots

--- cmp-adv-01-starting-setup/src/components/BadgeList.vue
+++ cmp-adv-01-starting-setup/src/components/BadgeList.vue
@@ -1,14 +1,18 @@
 <template>
   <base-card>
-    <h2>Available Badges</h2>
-    <ul>
-      <li>
-        <BaseBadge type="admin" caption="ADMIN" />
-      </li>
-      <li>
-        <BaseBadge type="author" caption="AUTHOR" />
-      </li>
-    </ul>
+    <template v-slot:header>
+      <h2>Available Badges</h2>
+    </template>
+    <template v-slot:default>
+      <ul>
+        <li>
+          <BaseBadge type="admin" caption="ADMIN" />
+        </li>
+        <li>
+          <BaseBadge type="author" caption="AUTHOR" />
+        </li>
+      </ul>
+    </template>
   </base-card>
 </template>
 
@@ -26,14 +30,6 @@ export default {
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
 section h2 {
   margin: 0.5rem 0;
   color: #3a3a3a;
