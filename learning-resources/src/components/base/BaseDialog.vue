7a88c83c019e2eb01d23ebd725b03d6aba216a74
Benedict <benedictiknkeonye@gmail.com>
2021-12-11 13:26:18 +0100

    Feat: adding the teleport feature

--- learning-resources/src/components/base/BaseDialog.vue
+++ learning-resources/src/components/base/BaseDialog.vue
@@ -1,22 +1,24 @@
 <template>
-  <section>
-    <div @click="$emit('close')"></div>
-    <dialog open>
-      <header>
-        <slot name="header">
-          <h2>{{ title }}</h2>
-        </slot>
-      </header>
-      <section>
-        <slot></slot>
-      </section>
-      <menu>
-        <slot name="actions">
-          <base-button @click="$emit('close')"></base-button>
-        </slot>
-      </menu>
-    </dialog>
-  </section>
+  <teleport to="body">
+    <section>
+      <div @click="$emit('close')"></div>
+      <dialog open>
+        <header>
+          <slot name="header">
+            <h2>{{ title }}</h2>
+          </slot>
+        </header>
+        <section>
+          <slot></slot>
+        </section>
+        <menu>
+          <slot name="actions">
+            <base-button @click="$emit('close')"></base-button>
+          </slot>
+        </menu>
+      </dialog>
+    </section>
+  </teleport>
 </template>
 <script>
 import BaseButton from './BaseButton.vue';
