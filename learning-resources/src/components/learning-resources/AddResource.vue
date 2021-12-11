077be93103bf73ea40f5d30c70560979ca89acb3
Benedict <benedictiknkeonye@gmail.com>
2021-12-11 13:09:32 +0100

    Feat: adding a custom dialog for error reporting

--- learning-resources/src/components/learning-resources/AddResource.vue
+++ learning-resources/src/components/learning-resources/AddResource.vue
@@ -1,37 +1,57 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitData">
-      <div class="form-control">
-        <label for="title">Title</label>
-        <input type="text" ref="titleInput" name="title" id="title" />
-      </div>
+  <section>
+    <base-dialog
+      v-if="inputIsInvalid"
+      :title="'Invalid Input'"
+      @close="confirmError"
+    >
+      <template #default>
+        <p>Unfortunately, at least one input value is invalid</p>
+        <p>
+          Please, check all inputs and make sure you enter at least a few
+          characters into each input field
+        </p>
+      </template>
+      <template #actions>
+        <base-button @click="confirmError">Okay</base-button>
+      </template>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitData">
+        <div class="form-control">
+          <label for="title">Title</label>
+          <input type="text" ref="titleInput" name="title" id="title" />
+        </div>
 
-      <div class="form-control">
-        <label for="description">Description</label>
-        <textarea
-          ref="descInput"
-          name="description"
-          id="description"
-          cols="30"
-          rows="3"
-        ></textarea>
-      </div>
+        <div class="form-control">
+          <label for="description">Description</label>
+          <textarea
+            ref="descInput"
+            name="description"
+            id="description"
+            cols="30"
+            rows="3"
+          ></textarea>
+        </div>
 
-      <div class="form-control">
-        <label for="link">Link</label>
-        <input ref="linkInput" type="url" id="link" name="link" />
-      </div>
+        <div class="form-control">
+          <label for="link">Link</label>
+          <input ref="linkInput" type="url" id="link" name="link" />
+        </div>
 
-      <div>
-        <base-button type="submit">Add Resource</base-button>
-      </div>
-    </form>
-  </base-card>
+        <div>
+          <base-button type="submit">Add Resource</base-button>
+        </div>
+      </form>
+    </base-card>
+  </section>
 </template>
 
 <script>
 import BaseCard from '@/components/base/BaseCard.vue';
 import BaseButton from '@/components/base/BaseButton.vue';
+import BaseDialog from '@/components/base/BaseDialog.vue';
+
 export default {
   name: 'AddResource',
   inject: ['addResource'],
@@ -39,6 +59,13 @@ export default {
   components: {
     BaseCard,
     BaseButton,
+    BaseDialog,
+  },
+
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
   },
 
   methods: {
@@ -47,7 +74,20 @@ export default {
       const description = this.$refs.descInput.value;
       const link = this.$refs.linkInput.value;
 
+      if (
+        title.trim() === '' ||
+        description.trim() === '' ||
+        link.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(title, description, link);
+    },
+
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
