c4871d822b33b436ce51a4421c6dd423622bf4df
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-01 10:59:16 +0100

    Feat: animating lists

---
+++ animations-01-starting-setup/src/components/UsersList.vue
@@ -0,0 +1,56 @@
+<template>
+<section>
+    <ul>
+        <li v-for="user of users" :key="`user-${user}`" @click="removeUser(user)">
+            {{ user }}
+        </li>
+    </ul>
+    <div>
+        <input type="text" ref="userNameInput">
+        <button @click="addUser">Add User</button>
+    </div>
+</section>
+</template>
+
+<script>
+export default {
+    name: 'UsersList',
+
+    data() {
+        return {
+            users: [
+                'Max',
+                'Ben',
+                'Lekan',
+                'Tayo',
+                'Tola'
+            ]
+        }
+    },
+
+    methods: {
+        addUser() {
+            const newUser = this.$refs.userNameInput.value;
+            this.users.unshift(newUser)
+        },
+
+        removeUser(user) {
+            this.users = this.users.filter(usr => usr !== user)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 1rem 0;
+        padding: 0;
+    }
+
+    li{
+        border: 1px solid #ccc;
+        padding: 1rem;
+        text-align: center;
+    }
+</style>
