53e23b29aab3208ccde60216981604cf6e578705
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-03 17:02:54 +0100

    Feat: adding authentication to state

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <base-container title="Vuex">
+<section>
+  <base-container title="Vuex" v-if="isLoggedInState">
     <TheCounter />
     <FavouriteValue />
-    <button @click="addOne">Add 20</button>
+    <button @click="increase({ value: 10})">Add 20</button>
     <ChangeCounter />
   </base-container>
+
+  <base-container title="auth">
+    <UserAuth />
+  </base-container>
+</section>
+
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 import FavouriteValue from './components/FavouriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
     ChangeCounter,
-    FavouriteValue
+    FavouriteValue,
+    UserAuth
+  },
+
+  computed: {
+    ...mapGetters(['isLoggedInState'])
   },
 
   methods: {
-    addOne() {
+    ...mapActions(['increase']),
+
+    // addOne() {
         // this.$store.commit('increase', { value: 10 })
         // this.$store.commit({
         //   type: 'increase',
@@ -30,11 +47,11 @@ export default {
         // })
 
         // To use actions, we call dispatch
-        this.$store.dispatch({
-          type: 'increase',
-          value: 10
-        })
-    }
+        // this.$store.dispatch({
+        //   type: 'increase',
+        //   value: 10
+        // })
+    // }
   }
 };
 </script>
---
+++ vuex-01-starting-setup/src/components/UserAuth.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+      <button @click="logUserIn" v-if="!isLoggedInState">Log in</button>
+      <button @click="logUserOut" v-if="isLoggedInState">Log out</button>
+  </section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+export default {
+    name: 'UserAuth',
+
+    computed: {
+        ...mapGetters(['isLoggedInState'])
+    },
+
+    methods: {
+        ...mapActions(['logUserIn', 'logUserOut'])
+    }
+}
+</script>
+
+<style>
+
+</style>
