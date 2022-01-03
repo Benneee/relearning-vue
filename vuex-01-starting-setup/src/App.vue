e62e3d30d828b4b706edb7845310795231b0ac4b
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-03 05:19:58 +0100

    Feat: passing a payload to mutations

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <base-container title="Vuex">
     <TheCounter />
-    <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
     <ChangeCounter />
   </base-container>
 </template>
@@ -20,7 +20,7 @@ export default {
 
   methods: {
     addOne() {
-        this.$store.commit('increment')
+        this.$store.commit('increase', { value: 10 })
     }
   }
 };
