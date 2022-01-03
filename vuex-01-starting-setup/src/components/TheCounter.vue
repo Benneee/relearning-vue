9ccf4d33920ed2aaad3b75d83287abf7e69e0c37
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-03 04:55:01 +0100

    Feat: creating the store and using the store's state

---
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,19 @@
+<template>
+      <h3> {{ $store.state.counter }} </h3>
+</template>
+
+<script>
+export default {
+    name: 'TheCounter',
+        
+    computed: {
+        counter() {
+            return this.$store.state.counter;
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
