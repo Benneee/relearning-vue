47914970c9e20bbc161699466530f4e3faacbd78
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-03 06:38:05 +0100

    Feat: using getters to access store data

---
+++ vuex-01-starting-setup/src/components/FavouriteValue.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h3> {{ counter }} </h3>
+        <p>We do more...</p>
+    </section>
+
+</template>
+
+<script>
+export default {
+    name: 'FavouriteValue',
+
+    computed: {
+        counter() {
+            return this.$store.getters.normalizedCounter;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
