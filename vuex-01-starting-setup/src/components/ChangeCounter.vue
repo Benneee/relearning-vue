873ed81e89fa47f405089d605c8eb0f6b8f4a06a
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-03 11:59:51 +0100

    Feat: using mappers

--- vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,14 +1,25 @@
 <template>
-    <button @click="addTwo">Add 2</button>
+    <section>
+            <button @click="increment">Add 2</button>
+            <button @click="increaseBy(11)">Add 11</button>
+    </section>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
     name: 'ChangeCounter',
 
     methods: {
-        addTwo() {
-            this.$store.dispatch('increment')
+        // addTwo() {
+        //     this.$store.dispatch('increment')
+        // }
+
+        ...mapActions(['increment', 'increase']),
+
+        increaseBy(amount) {
+            return this.increase({ value: amount })
+             
         }
     }
 }
--- vuex-01-starting-setup/src/components/TheCounter.vue
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -1,15 +1,20 @@
 <template>
-      <h3> {{ counter }} </h3>
+      <h3> {{ finalCounter }} </h3>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
     name: 'TheCounter',
         
     computed: {
-        counter() {
-            return this.$store.getters.finalCounter;
-        }
+        // counter() {
+        //     return this.$store.getters.finalCounter;
+        // }
+
+        // This grants the component access to the finalCounter getter immediately
+        ...mapGetters(['finalCounter'])
     },
 }
 </script>
