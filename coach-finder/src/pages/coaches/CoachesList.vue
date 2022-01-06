71089dd97b8daac0add127e55ae0e1abb818ad1c
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-06 16:32:11 +0100

    Feat: provision store with dummy data

--- coach-finder/src/pages/coaches/CoachesList.vue
+++ coach-finder/src/pages/coaches/CoachesList.vue
@@ -6,16 +6,30 @@
         <button>Refresh</button>
         <router-link to="/register">Register as coach </router-link>
       </div>
-      <ul>
+      <ul v-if="hasCoaches">
         List of coaches
+        <li v-for="coach of filteredCoaches" :key="coach.id">
+          {{ coach.firstName }}
+        </li>
       </ul>
+      <h3 v-else>No coaches found</h3>
     </div>
   </section>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'CoachesList',
+
+  computed: {
+    ...mapGetters('coaches', ['coaches', 'hasCoaches']),
+
+    filteredCoaches() {
+      return this.coaches;
+    },
+  },
 };
 </script>
 
