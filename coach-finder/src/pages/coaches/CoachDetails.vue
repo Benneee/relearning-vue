6b312632f6fec32f3442205175935151d90d4673
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-06 19:44:16 +0100

    Feat: coach details page

--- coach-finder/src/pages/coaches/CoachDetails.vue
+++ coach-finder/src/pages/coaches/CoachDetails.vue
@@ -1,14 +1,91 @@
 <template>
   <section>
-    Details for Coach
-    <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+    <div>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h4>${{ rate }}/hour</h4>
+      </base-card>
+    </div>
+
+    <div>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </div>
+
+    <div>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area"
+          :title="area"
+          :type="area"
+        >
+        </base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </div>
   </section>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'CoachDetails',
+
+  props: {
+    id: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+
+  computed: {
+    ...mapGetters('coaches', ['coaches']),
+
+    fullName() {
+      return `${this.selectedCoach.firstName} ${this.selectedCoach.lastName}`;
+    },
+
+    contactLink() {
+      return `${this.$route.path}/contact`;
+    },
+
+    areas() {
+      return this.selectedCoach.areas;
+    },
+
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+
+    description() {
+      return this.selectedCoach.description;
+    },
+  },
+
+  created() {
+    this.getSelectedCoach();
+  },
+
+  methods: {
+    getSelectedCoach() {
+      if (this.coaches.length > 0) {
+        this.selectedCoach = this.coaches.find((coach) => coach.id === this.id);
+      }
+    },
+  },
 };
 </script>
 
