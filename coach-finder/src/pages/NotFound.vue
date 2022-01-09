0d10c8e14f3d893de18efaa5556e88abee84577b
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-09 19:17:09 +0100

    Feat: added the markup for the NotFound page

--- coach-finder/src/pages/NotFound.vue
+++ coach-finder/src/pages/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
   <section>
-    <p>You seem lost, please, find your way home</p>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>
+        You seem lost, please, find your way to our
+        <router-link to="/coaches">coaches</router-link> page
+      </p>
+    </base-card>
   </section>
 </template>
 
