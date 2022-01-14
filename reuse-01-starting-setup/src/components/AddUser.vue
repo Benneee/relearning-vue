29636526b063c5823be72d9139b16e8deeb8aead
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-14 17:52:51 +0100

    Feat: using mixins

--- reuse-01-starting-setup/src/components/AddUser.vue
+++ reuse-01-starting-setup/src/components/AddUser.vue
@@ -1,32 +1,22 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
-    <p>Do you want to continue with adding a user?</p>
-  </user-alert>
   <section>
-    <h2>Add a User</h2>
-    <button @click="showAlert">Add User</button>
+    <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+      <p>Do you want to continue with adding a user?</p>
+    </user-alert>
+    <div>
+      <h2>Add a User</h2>
+      <button @click="showAlert">Add User</button>
+    </div>
   </section>
 </template>
 
 <script>
 import UserAlert from './UserAlert.vue';
 
+import AlertMixin from '../mixins/alert';
+
 export default {
-  components: {
-    UserAlert,
-  },
-  data() {
-    return {
-      alertIsVisible: false,
-    };
-  },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
-  },
+  components: { UserAlert },
+  mixins: [AlertMixin],
 };
 </script>
--- reuse-01-starting-setup/src/components/DeleteUser.vue
+++ reuse-01-starting-setup/src/components/DeleteUser.vue
@@ -1,32 +1,25 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
-    <p>Do you want to continue with deleting a user?</p>
-  </user-alert>
   <section>
-    <h2>Delete a User</h2>
-    <button @click="showAlert">Delete User</button>
+    <user-alert
+      v-if="alertIsVisible"
+      title="Delete the User?"
+      @close="hideAlert"
+    >
+      <p>Do you want to continue with deleting a user?</p>
+    </user-alert>
+    <div>
+      <h2>Delete a User</h2>
+      <button @click="showAlert">Delete User</button>
+    </div>
   </section>
 </template>
 
 <script>
 import UserAlert from './UserAlert.vue';
+import AlertMixin from '../mixins/alert';
 
 export default {
-  components: {
-    UserAlert,
-  },
-  data() {
-    return {
-      alertIsVisible: false,
-    };
-  },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
-  },
+  components: { UserAlert },
+  mixins: [AlertMixin],
 };
 </script>
