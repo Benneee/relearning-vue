2259fb1457f3abc7e6ecbf9fc415163cbf042e2d
Benedict Nkeonye <benedictnkeonye@Benedicts-MacBook-Pro.local>
2022-01-17 13:46:48 +0100

    Feat: reusability in composition API

---
+++ reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <user-alert
+      v-if="alertIsVisible"
+      title="Delete the User?"
+      @close="hideAlert"
+    >
+      <p>Do you want to continue with deleting a user?</p>
+    </user-alert>
+    <div>
+      <h2>Delete a User</h2>
+      <button @click="showAlert">Delete User</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref } from 'vue';
+import UserAlert from './UserAlert.vue';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  setup() {
+    const alertIsVisible = ref(false);
+
+    function showAlert() {
+      alertIsVisible.value = true;
+    }
+    function hideAlert() {
+      alertIsVisible.value = false;
+    }
+
+    return {
+      alertIsVisible,
+      showAlert,
+      hideAlert,
+    };
+  },
+};
+</script>
